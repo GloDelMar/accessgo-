e0a53e026622aea45a7331f437c00a4f15fb02b7
Gloriela Suarez <glo.suacas@gmail.com>
2024-10-17 21:12:03 -0600

    Resolver conflictos en pages/5/autentificacion.jsx y modificar notificacion13.jsx

--- pages/5/autentificacion.jsx
+++ pages/5/autentificacion.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from 'next/router';
 
+
 const View5 = () => {
   const router = useRouter();
   const [tipoUsuario, setTipoUsuario] = useState("");
@@ -30,24 +31,24 @@ const View5 = () => {
   return (
     <>
       <div className='max-w-md w-full mx-auto'>
-        <h1 className='text-center text-2xl text-[#2F4F4F] p-10 font-bold'>
+      <h1 className='text-center text-2xl text-[#2F4F4F] p-10 font-bold'>
           ¡Revisa tu correo o tu WhatsApp!
         </h1>
         <h3 className='text-center text-[#2F4F4F] p-10 font-bold'>
           Te hemos enviado un código de verificación. A continuación, escribe el código que te enviamos para validar tu cuenta.
         </h3>
 
-        <div className='text-center flex justify-center p-40'>
+        <div className='text-center flex justify-center p-40 '>
           <div>
             <div>
-              <a className='text-center flex justify-center text-[#2F4F4F] p-10 font-bold'>
+            <a className='text-center flex justify-center text-[#2F4F4F] p-10 font-bold'>
                 Confirma Tu Código
               </a>
               <input
                 type='text'
                 name='codigo'
                 placeholder='Código de verificación'
-                className='px-10 py-2 border bg-[#F6F9FF] text-center rounded-md text-sm font-medium text-[#546E7A] hover:bg-[#ECEFF1] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#B0BEC5]'
+                  className='px-10 py-2 border bg-[#F6F9FF] text-center rounded-md text-sm font-medium text-[#546E7A] hover:bg-[#ECEFF1] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#B0BEC5] '
               />
             </div>
           </div>
@@ -63,10 +64,12 @@ const View5 = () => {
           </p>
         )}
 
-        <div className='flex justify-center items-center py-5'>
+<div className='flex justify-center items-center py-5'>
           <button
             onClick={handleRedirect}
-            className='px-6 py-2 border border-transparent rounded-md shadow-sm text-white bg-[#2F4F4F] hover:bg-[#004D40] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#00695C]'
+            className='px-6 py-2 border border-transparent rounded-md shadow-sm
+            text-white bg-[#2F4F4F] hover:bg-[#004D40] focus:outline-none
+            focus:ring-2 focus:ring-offset-2 focus:ring-[#00695C]'
           >
             Iniciar Sesión
           </button>
@@ -77,3 +80,4 @@ const View5 = () => {
 };
 
 export default View5;
+
