0f9ff9ab6c1c8ada7765e1abb79c96ded5cbf737
Raul Elizalde <raulei2323@gmail.com>
2024-11-10 03:13:19 -0600

    aun no muestra imagen del bucket

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['encrypted-tbn0.gstatic.com'],
+    domains: ['encrypted-tbn0.gstatic.com', 'accessgo-test-s3-bucket.s3.amazonaws.com' ],
+    
+
   },
 };
 
---
+++ pages/testaws.jsx
@@ -0,0 +1,9 @@
+import UploadImage from "@/components/Molecules/UploadImage";
+
+export default function testaws() {
+  return (
+      <div className='w-full h-full flex-col p-2  '>
+        <UploadImage/>
+      </div>
+  );
+}
