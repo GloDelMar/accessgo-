64ec46ae483f897b1abdaa7bd80f2fc16a3aa636
Gloriela Suarez <glo.suacas@gmail.com>
2024-11-19 15:58:34 -0600

    Ultimos ajustes

--- components/Organism/Checkpoints.jsx
+++ components/Organism/Checkpoints.jsx
@@ -1,48 +1,35 @@
-import MotrizCheckpoints from './Hotel/MotrizCheckpoints';
-import VisualCheckpoints from './Hotel/VisualCheckpoints';
-import AuditivaCheckpoints from './Hotel/AuditivaCheckpoints';
-import IntelectualCheckpoints from './Hotel/IntelectualCheckpoints';
-import NeurodivergenteCheckpoints from './Hotel/NeurodivergenteCheckpoints';
-import RestMotrizCheckpoints from './Restaurante/MotrizCheckpoints';
-import RestVisualCheckpoints from './Restaurante/VisualCheckpoints';
-import RestAuditivaCheckpoints from './Restaurante/AuditivaCheckpoints';
-import RestIntelectualCheckpoints from './Restaurante/IntelectualChecpoints';
-import RestNeurodivergenteCheckpoints from './Restaurante/NeurodivergenteCheckpoints';
+import React from "react";
 
-const Checkpoints = ({ condition, establishment, accessibilityData, setAccessibilityData }) => {
-  if (establishment === 'hotel') {
-    switch (condition) {
-      case 'Motriz':
-        return <MotrizCheckpoints accessibilityData={accessibilityData} setAccessibilityData={setAccessibilityData} />;
-      case 'Visual':
-        return <VisualCheckpoints accessibilityData={accessibilityData} setAccessibilityData={setAccessibilityData} />;
-      case 'Auditiva':
-        return <AuditivaCheckpoints accessibilityData={accessibilityData} setAccessibilityData={setAccessibilityData} />;
-      case 'Intelectual':
-        return <IntelectualCheckpoints accessibilityData={accessibilityData} setAccessibilityData={setAccessibilityData} />;
-      case 'Neurodivergente':
-        return <NeurodivergenteCheckpoints accessibilityData={accessibilityData} setAccessibilityData={setAccessibilityData} />;
-      default:
-        return <p>Selecciona un tipo de discapacidad para ver los checkpoints.</p>;
-    }
-  } else if (establishment === 'restaurante') {
-    switch (condition) {
-      case 'Motriz':
-        return <RestMotrizCheckpoints accessibilityData={accessibilityData} setAccessibilityData={setAccessibilityData} />;
-      case 'Visual':
-        return <RestVisualCheckpoints accessibilityData={accessibilityData} setAccessibilityData={setAccessibilityData} />;
-      case 'Auditiva':
-        return <RestAuditivaCheckpoints accessibilityData={accessibilityData} setAccessibilityData={setAccessibilityData} />;
-      case 'Intelectual':
-        return <RestIntelectualCheckpoints accessibilityData={accessibilityData} setAccessibilityData={setAccessibilityData} />;
-      case 'Neurodivergente':
-        return <RestNeurodivergenteCheckpoints accessibilityData={accessibilityData} setAccessibilityData={setAccessibilityData} />;
-      default:
-        return <p>Selecciona un tipo de discapacidad para ver los checkpoints.</p>;
-    }
-  }
+const CheckpointsSection = ({ sections, onUpdate }) => {
+  const handleChange = (sectionIndex, questionIndex) => {
+    const updatedSections = [...sections];
+    updatedSections[sectionIndex].questions[questionIndex].response =
+      !updatedSections[sectionIndex].questions[questionIndex].response;
+    onUpdate(updatedSections);
+  };
 
-  return <p>Selecciona un tipo de establecimiento.</p>;
+  return (
+    <div>
+      {sections.map((section, sectionIndex) => (
+        <div key={sectionIndex} className="mb-4">
+          <h2 className="font-bold">{section.name}</h2>
+          <ul>
+            {section.questions.map((question, questionIndex) => (
+              <li key={questionIndex} className="flex items-center">
+                <input
+                  type="checkbox"
+                  checked={question.response}
+                  onChange={() => handleChange(sectionIndex, questionIndex)}
+                  className="mr-2"
+                />
+                {question.question}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 };
 
-export default Checkpoints;
+export default CheckpointsSection;
