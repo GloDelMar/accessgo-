893bc4ec2fbdc16eec83c871e46e34448638c6b8
ald0u <aldoulaguent@gmail.com>
2024-11-24 22:27:12 -0600

    Voluntariado y se remueve icono del mapa

--- pages/voluntariado.jsx
+++ pages/voluntariado.jsx
@@ -3,22 +3,12 @@
 import React, { useState, useEffect } from 'react';
 import { toast, Toaster } from 'react-hot-toast';
 import { createVolunteer, getAllVolunteers } from './api/api_volunteer';
+import { useRouter } from 'next/router';
 
 const Voluntario = () => {
-  const [association, setAssociation] = useState({
-    name: '',
-    address: '',
-    contact: '',
-    activities: '',
-  });
-
   const [volunteers, setVolunteers] = useState([]);
-  const [submitted, setSubmitted] = useState(false);
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setAssociation((prev) => ({ ...prev, [name]: value }));
-  };
+  const router = useRouter();
+  const handleRedireccion = () => router.push('/solicitud-voluntario');
 
   const fetchVolunteers = async () => {
     try {
@@ -28,29 +18,6 @@ const Voluntario = () => {
       console.error('Error al obtener voluntarios:', error);
     }
   };
-
-  const handleSubmit = async () => {
-    try {
-      const response = await createVolunteer(association);
-      if (response.success) {
-        setSubmitted(true);
-        setAssociation({
-          name: '',
-          address: '',
-          contact: '',
-          activities: '',
-        });
-        toast.success('Participación exitosa');
-        fetchVolunteers();
-      } else {
-        toast.error('Error al enviar los datos');
-      }
-    } catch (error) {
-      console.error('Error al enviar los datos:', error);
-      toast.error('Error en la solicitud');
-    }
-  };
-
 
   useEffect(() => {
     fetchVolunteers();
@@ -66,49 +33,6 @@ const Voluntario = () => {
       <p className="text-lg text-center">
         <strong>En AccesoGo,</strong> creemos que ser voluntario es una manera más de apoyar a las Personas con Discapacidad y contribuir a una Comunidad Inclusiva.
       </p>
-
-      <section>
-        <h2 className="text-2xl font-semibold mb-4">Detalles de la Asociación</h2>
-        <div className="grid grid-cols-1 gap-4">
-          <input
-            name="name"
-            placeholder="Nombre de la asociación"
-            value={association.name}
-            onChange={handleInputChange}
-            className="w-full px-4 py-2 border bg-[#F9F9F9] rounded-md text-sm font-medium text-[#546E7A] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#B0BEC5]"
-          />
-          <input
-            name="address"
-            placeholder="Dirección"
-            value={association.address}
-            onChange={handleInputChange}
-            className="w-full px-4 py-2 border bg-[#F9F9F9] rounded-md text-sm font-medium text-[#546E7A] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#B0BEC5]"
-          />
-          <input
-            name="contact"
-            placeholder="Contacto"
-            value={association.contact}
-            onChange={handleInputChange}
-            className="w-full px-4 py-2 border bg-[#F9F9F9] rounded-md text-sm font-medium text-[#546E7A] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#B0BEC5]"
-          />
-          <textarea
-            name="activities"
-            placeholder="Actividades que requieren voluntarios"
-            value={association.activities}
-            onChange={handleInputChange}
-            className="w-full px-4 py-2 border bg-[#F9F9F9] rounded-md text-sm font-medium text-[#546E7A] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#B0BEC5] h-32 resize-none"
-          />
-        </div>
-      </section>
-
-      <section>
-        <button
-          onClick={handleSubmit}
-          className="px-6 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600"
-        >
-          Participar
-        </button>
-      </section>
 
       <section>
         <h2 className="text-2xl font-semibold mb-4">Voluntarios</h2>
@@ -131,6 +55,14 @@ const Voluntario = () => {
         </div>
       </section>
 
+      <section>
+        <button
+          onClick={handleRedireccion}
+          className="px-6 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600"
+        >
+          Solicitar voluntario
+        </button>
+      </section>
 
       <section>
         <h2 className="text-2xl font-semibold mb-4">¿Cómo participar?</h2>
