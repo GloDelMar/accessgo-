345f933a31bdbcbb2b38fb13043b967ddb6cfacf
David2024xps <David2024xps>
2024-12-12 20:57:50 -0600

    Se actulizo el recuadro de imagen de perfil de usuario

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,32 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['encrypted-tbn0.gstatic.com', 'accessgo-test-s3-bucket.s3.amazonaws.com'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'cdn.builder.io',
         pathname: '/api/v1/image/assets/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'accessgo-test-s3-bucket.s3.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
-    dangerouslyAllowSVG: true, 
+    dangerouslyAllowSVG: true,
   },
 };
 
