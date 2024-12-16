c7548959783a22adfa636a3633a5f46d8cabe17b
Gloriela Suarez <glo.suacas@gmail.com>
2024-12-15 23:57:25 -0600

    filtros funcionando, por discapacidad y por mejor calificado, impresión de íconos de accesibilidad  en modal

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,20 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['encrypted-tbn0.gstatic.com', 'accessgo-test-s3-bucket.s3.amazonaws.com'],
+    domains: ['encrypted-tbn0.gstatic.com', 'accessgo-test-s3-bucket.s3.amazonaws.com'], // Dominios permitidos
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'cdn.builder.io',
-        pathname: '/api/v1/image/assets/**',
+        hostname: 'encrypted-tbn0.gstatic.com', // Asegúrate de las configuraciones específicas
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'accessgo-test-s3-bucket.s3.amazonaws.com',
+        pathname: '**',
       },
     ],
-    dangerouslyAllowSVG: true, 
+    dangerouslyAllowSVG: true, // Opcional si necesitas permitir SVG
   },
 };
 
