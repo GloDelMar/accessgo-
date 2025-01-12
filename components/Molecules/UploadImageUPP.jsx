499c828728c8670bda75271b750cb1836ad80fbb
Raul Elizalde <raulei2323@gmail.com>
2025-01-11 22:13:00 -0600

    subida sin botones

--- components/Molecules/UploadImageUPP.jsx
+++ components/Molecules/UploadImageUPP.jsx
@@ -40,8 +40,12 @@ const UploadImageUPP = ({ userId }) => {
     fetchUserData();
   }, [userId]);
 
-  const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
+  const handleFileChange = async (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setFile(file);
+      await handleUpload(file); 
+    }
   };
 
   const handleUpload = async () => {
@@ -91,7 +95,10 @@ const UploadImageUPP = ({ userId }) => {
   return (
     <div className='flex flex-col justify-center items-center space-y-4 '>
       <h3 className='text-md text-center font-semibold'>Imagen de perfil</h3>
-      <div className='flex justify-center lg:justify-start'>
+      <div
+        className='flex justify-center lg:justify-start'
+        onClick={() => document.getElementById('fileInput').click()}
+      >
         <label htmlFor='imgUsuario' className='cursor-pointer'>
           {selectedImage ? (
             <Image
@@ -113,29 +120,12 @@ const UploadImageUPP = ({ userId }) => {
         </label>
       </div>
 
-      <label
-        htmlFor='fileInput'
-        className='px-6 py-2 border border-transparent rounded-md shadow-sm text-white bg-[#2F4F4F] hover:bg-[#004D40] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#00695C]'
-      >
-        {file ? file.name : 'Seleccionar'}
-      </label>
-
       <input
         type='file'
         id='fileInput'
         className='hidden'
-        onChange={handleFileChange}
+        onChange={handleFileChange} 
       />
-
-      <button
-        onClick={handleUpload}
-        disabled={uploading}
-        className={`px-6 py-2 border border-transparent rounded-md shadow-sm text-white bg-[#2F4F4F] hover:bg-[#004D40] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#00695C] ${
-          uploading ? 'bg-gray-400' : 'bg-blue-600 hover:bg-blue-500'
-        } focus:outline-none`}
-      >
-        {uploading ? 'Subiendo...' : 'Subir'}
-      </button>
       <Toaster />
     </div>
   );
