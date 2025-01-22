565d146a406ad8ce83983dc60d8b33a1d8aa5adf
Raul Elizalde <raulei2323@gmail.com>
2025-01-21 21:35:40 -0600

    on dev

--- components/Molecules/ImageCarouselACC.jsx
+++ components/Molecules/ImageCarouselACC.jsx
@@ -1,27 +1,22 @@
-import { useState, useEffect } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.css';
+import React, { useState, useEffect } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react'; // Usa Swiper y SwiperSlide de 'swiper/react'
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation'; // Importa los estilos necesarios
+import SwiperCore, { Autoplay, Navigation, Pagination } from 'swiper';
 
-
+// Configura Swiper para usar los módulos necesarios
+SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 const ImageCarouselACC = ({ userId }) => {
-  
   const [images, setImages] = useState([]);
   const [selectedImage, setSelectedImage] = useState(null);
-  const [currentSlide, setCurrentSlide] = useState(0);
 
   useEffect(() => {
     const fetchImages = async () => {
-      const response = await fetch(
-        `https://backend-r159.onrender.com/api/images/${userId}`
-      );
+      const response = await fetch(`https://backend-r159.onrender.com/api/images/${userId}`);
       const data = await response.json();
-      if (Array.isArray(data)) {
-        setImages(data);
-      } else {
-        console.error('Expected an array, but got:', data);
-        setImages([]);
-      }
+      setImages(Array.isArray(data) ? data : []);
     };
     fetchImages();
   }, [userId]);
@@ -34,41 +29,34 @@ const ImageCarouselACC = ({ userId }) => {
     setSelectedImage(null);
   };
 
-  
-
   return (
-    <div className='flex justify-center md:w-[750px] lg:w-[900px]' >
+    <div className="w-full max-w-[1022px] mx-auto">
       {images.length > 0 ? (
         <Swiper
-          pagination={{
-            clickable: images.length > 1,
+          loop={true}
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
           }}
+          speed={500}
           spaceBetween={20}
-          slidesPerView={1}
-          loop={images.length > 1}
-          centeredSlides={images.length === 1}
+          slidesPerView="auto"
+          centeredSlides={true}
+          pagination={{ clickable: true }}
+          navigation // Activa la navegación
           breakpoints={{
-            
-            320: {
-              slidesPerView: 1
-            },
-            768: {
-              slidesPerView: Math.min(images.length, 2)
-            },
-            1024: {
-              slidesPerView: Math.min(images.length, 4)
-            }
+            640: { slidesPerView: 1 },
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 4 },
           }}
-          className={`flex ${
-            images.length === 1 ? 'justify-center' : ''
-          } flex-col justify-self-center max-w-[250px] md:max-w-[450px] lg:max-w-[900px] h-full`}
+          className="flex justify-center items-center w-full h-[200px]"
         >
           {images.map((image, index) => (
             <SwiperSlide key={index}>
               <img
                 src={image}
                 alt={`Slide ${index}`}
-                className='justify-self-center w-[200px] h-[200px] object-cover rounded-lg cursor-pointer'
+                className="w-[200px] h-[150px] object-cover rounded-lg cursor-pointer"
                 onClick={() => handleImageClick(image)}
               />
             </SwiperSlide>
@@ -79,16 +67,16 @@ const ImageCarouselACC = ({ userId }) => {
       )}
 
       {selectedImage && (
-        <div className='fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50'>
-          <div className='relative bg-white p-4 rounded-lg'>
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+          <div className="relative bg-white p-4 rounded-lg">
             <img
               src={selectedImage}
-              alt='Selected Image'
-              className='w-[80vw] h-[80vh] object-contain'
+              alt="Selected Image"
+              className="w-[80vw] h-[80vh] object-contain"
             />
             <button
               onClick={handleCloseModal}
-              className='absolute top-0 right-0 p-2 text-white bg-red-600 rounded-full'
+              className="absolute top-0 right-0 p-2 text-white bg-red-600 rounded-full"
             >
               X
             </button>
