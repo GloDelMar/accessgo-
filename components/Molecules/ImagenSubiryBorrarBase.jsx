5476afe04cc8f51c54cfe3149593d2cd82462f5a
Raul Elizalde <raulei2323@gmail.com>
2025-02-07 18:44:13 -0600

    quality added

--- components/Molecules/ImagenSubiryBorrarBase.jsx
+++ components/Molecules/ImagenSubiryBorrarBase.jsx
@@ -47,7 +47,7 @@ const ImagenSubiryBorrarBase = ({ userId }) => {
 
     try {
       const response = await fetch(
-        'http://localhost:8080/api/uploadacc',
+        'https://backend-r159.onrender.com/api/uploadacc',
         {
           method: 'POST',
           body: formData
