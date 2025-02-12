669142c753ed3535d5fb79930e397827927a5065
Raul Elizalde <raulei2323@gmail.com>
2025-02-11 22:46:29 -0600

    Calidad de imagen ajustada, minificacion de redux arreglada(Aun por verificar

--- components/Molecules/ImageCarouselACC.jsx
+++ components/Molecules/ImageCarouselACC.jsx
@@ -33,10 +33,8 @@ const ImageCarouselACC = ({ userId }) => {
   };
 
   return (
-    <div className='flex flex-col justify-self-center items-center md:w-[750px] lg:w-[900px]' >
-      
+    <div className='flex justify-self-center md:w-[750px] lg:w-[900px]' >
       {images.length > 0 ? (
-        
         <Swiper
           modules={[Autoplay, Pagination]} 
           pagination={{
@@ -63,7 +61,7 @@ const ImageCarouselACC = ({ userId }) => {
           }}
           className={`flex ${
             images.length === 1 ? 'justify-center' : ''
-          } flex-col justify-self-center max-w-[250px] md:max-w-[450px] lg:max-w-[900px] h-full mt-8`}
+          } flex-col justify-self-center max-w-[250px] md:max-w-[450px] lg:max-w-[900px] h-full`}
         >
           {images.map((image, index) => (
             <SwiperSlide key={index}>
@@ -81,12 +79,11 @@ const ImageCarouselACC = ({ userId }) => {
           ))}
         </Swiper>
       ) : (
-        <p></p>
+        <p>No images found</p>
       )}
 
       {selectedImage && (
         <div  className='max-w-screen max-h-screen fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50'>
-          
           <div className='w-full h-full relative bg-white p-4 rounded-lg'>
             <Image
               src={selectedImage}
