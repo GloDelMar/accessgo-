8d6df695611d2aa8171c2630a3e3576f60aa949e
Gloriela Suarez <glo.suacas@gmail.com>
2025-02-23 23:45:14 -0600

    modal responsivo

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -7,20 +7,11 @@ const Modal = ({ onClose, children }) => {
       <div className="fixed inset-0 bg-black bg-opacity-50 z-40" onClick={onClose}></div>
 
       {/* Contenedor del modal */}
-      <div className="fixed inset-0 flex items-center justify-center z-50">
-        <div className="bg-white rounded-lg shadow-lg w-11/12 md:w-1/3 p-6">
-          {/* Contenido dinámico que pasamos como `children` */}
-          {children}
-
-          {/* Botón para cerrar el modal */}
-          <div className="flex justify-end mt-4">
-            <button
-              onClick={onClose}
-              className="hidden px-4 py-2 bg-[#2F4F4F] text-white rounded-md hover:bg-[#004D40]"
-            >
-              Cerrar
-            </button>
-          </div>
+      <div className="fixed inset-0 flex items-center justify-center z-50 p-4">
+        <div className="bg-white items-center rounded-lg shadow-lg w-full max-w-[600px] md:max-w-[700px] lg:max-w-[800px]  flex flex-col p-6">
+          {/* Contenido dinámico */}
+          <div className="flex-grow">{children}</div>
+      
         </div>
       </div>
     </>
